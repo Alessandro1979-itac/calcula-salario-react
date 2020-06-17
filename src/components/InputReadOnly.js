c9e2d24bb5e17deb788493147b3544cde8e6ad9d
Alessandro1979-itac <muniz.caranha@gmail.com>
2020-06-17 16:43:54 -0400

    Melhorias na escrita do código

--- src/components/InputReadOnly.js
+++ src/components/InputReadOnly.js
@@ -3,20 +3,24 @@ import { formatPercentage, formatMoney } from '../helpers/formatters';
 
 export default class InputReadOnly extends Component {
   render() {
-    const { color = 'black', value, percentage = 0, label } = this.props;
+    const { color = 'black', value, percentage, label } = this.props;
+
     const id = 'input' + label;
-    const formattedPercentage = percentage > 0 ? `(${formatPercentage(percentage)})` : '';
-    const formattedValue = `${formatMoney(value)} ${formattedPercentage}`;
+    let formattedValue = formatMoney(value);
+    formattedValue += percentage > 0 ? ' ' + formatPercentage(percentage) : '';
 
     return (
       <div className="input-field col s12 m6 l3">
         <input
           id={id}
+          type="text"
           value={formattedValue}
-          readOnly
           style={{ color, fontWeight: 'bold' }}
+          readOnly
         />
-        <label className="active" htmlFor={id}>{label}</label>
+        <label className="active" htmlFor={id}>
+          {label}
+        </label>
       </div>
     );
   }
